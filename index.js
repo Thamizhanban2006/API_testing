645e1370ef4fe9bdb05feff273236592338819bf
ThamizhAnban <thamizhanban2006@gmail.com>
2025-02-05 12:03:16 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,51 +1,47 @@
-// API: Retrieve Students Above Threshold
-// ---------------------------------------
-// Task:
-// Implement an API to fetch students whose total marks exceed a given threshold.
-//
-// Endpoint:
-// POST /students/above-threshold
-//
-// Request Body:
-// {
-//   "threshold": <number>
-// }
-//
-// Response:
-// Success: List of students with their names and total marks who meet the criteria.
-// Example:
-// {
-//   "count": 2,
-//   "students": [
-//     { "name": "Alice Johnson", "total": 433 },
-//     { "name": "Bob Smith", "total": 410 }
-//   ]
-// }
-//
-// No Matches:
-// {
-//   "count": 0,
-//   "students": []
-// }
-//
-// Purpose:
-// Help teachers retrieve and analyze student performance efficiently.
-
 
 const express = require('express');
 const { resolve } = require('path');
-
+const data = require('./data.json')
 const app = express();
-const port = 3010;
+const port = 3001;
 
 app.use(express.static('static'));
 
+app.use(express.json());
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+// app.get('/students',(req,res)=>{
+//   res.send(res);
+// })
+
+app.post("/students/above-threshold",(req,res)=>{
+  const {threshold} = req.body;
+
+
+const filteredStudents = data
+  .filter(student => student.total >=parseInt(threshold)) // Filtering based on total marks
+  .map(student => ({
+    name: student.name,
+    total: student.total
+  }));
+
+
+  const output = {
+    count: filteredStudents.length,
+    students: filteredStudents
+  };
+
+  res.json(
+        output
+    )
+//   
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-
+// console.log(data);
